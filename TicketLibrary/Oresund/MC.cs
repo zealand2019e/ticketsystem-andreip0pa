5378f393706f5f8ced44c0d72f52e51de9b5b140
Andrei Popa <andreip0pa44@gmail.com>
2019-09-20 13:17:21 +0200

    MC

---
+++ TicketLibrary/Oresund/MC.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oresund
+{
+   public class MC:TicketLibrary.MC;
+    {
+        bool brobizz = false;
+
+        public override double Price()
+        {
+            double price = 210;
+            if (brobizz)
+            {
+                price = 73;
+            }
+            return price;
+        }
+        public override string VehicleType()
+        {
+            return "Oresund MC";
+        }
+    }
+}
