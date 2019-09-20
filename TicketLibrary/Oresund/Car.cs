ae937bc10bf58e8a61b1c096bb08968ac89f350d
Andrei Popa <andreip0pa44@gmail.com>
2019-09-20 13:15:18 +0200

    Ores

---
+++ TicketLibrary/Oresund/Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Oresund
+{
+    public class Car:TicketLibrary.Car
+    {
+        bool brobizz = false;
+
+        public override double Price()
+        {
+            double price = 410;
+            if (brobizz)
+            {
+                price = 161;
+            }
+            return price;
+        }
+        public override string VehicleType()
+        {
+            return "Oresund Car";
+        }
+    }
+}
--- TicketLibrary/TicketLibrary/Car.cs
+++ TicketLibrary/TicketLibrary/Car.cs
@@ -11,10 +11,7 @@ namespace TicketLibrary
         public override double Price()
         {
             double price = 240;
-            if (DateTime.Today.DayOfWeek==DayOfWeek.Saturday || DateTime.Today.DayOfWeek == DayOfWeek.Sunday)
-            {
-                price -= price * 20 / 100;
-            }
+          
             if (Brobizz)
             {
                 price -= price * 5 / 100;
